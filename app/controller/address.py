d0c2083f6f444bd6eb5c1f6e5b3c6d199f3079b2
Abhishek Jha <103423554+coderphile@users.noreply.github.com>
2023-10-17 01:37:30 +0530

    Update address.py

--- app/controller/address.py
+++ app/controller/address.py
@@ -7,34 +7,38 @@ from fastapi import Depends, HTTPException, Query
 
 router = APIRouter()
 
-@router.get("/", response_model=List[schemas.Address])
+
+
+
+@router.get("/")
 def get_addresses(db: Session = Depends(database.get_db)):
     db_addresses = adr.get_addresses(db)
     if not db_addresses:
         raise HTTPException(status_code=404, detail="No address found!")
     return db_addresses
 
+
 @router.post("/", response_model=schemas.Address)
 def create_address(address: schemas.AddressCreate, db: Session = Depends(database.get_db)):
     db_address = adr.create_address(db, address)
     return db_address
 
-
-@router.get("/{address_id}", response_model=schemas.Address)
+@router.get("/find/{address_id}", response_model=schemas.Address)
 def read_address(address_id: int, db: Session = Depends(database.get_db)):
     db_address = adr.get_address_by_id(db, address_id)
     if db_address is None:
         raise HTTPException(status_code=404, detail="Address not found")
     return db_address
 
-@router.put("/{address_id}", response_model=schemas.Address)
+@router.put("/update/{address_id}", response_model=schemas.Address)
 def update_address(address_id: int, address: schemas.AddressCreate, db: Session = Depends(database.get_db)):
     db_address = adr.update_address(db, address_id, address)
     if db_address is None:
         raise HTTPException(status_code=404, detail="Address not found")
     return db_address
 
-@router.delete("/{address_id}", response_model=schemas.Address)
+
+@router.delete("/delete/{address_id}", response_model=schemas.Address)
 def delete_address(address_id: int, db: Session = Depends(database.get_db)):
     db_address = adr.delete_address(db, address_id)
     if db_address is None:
@@ -50,5 +54,6 @@ def get_addresses_within_distance(
     db: Session = Depends(database.get_db)
 ):
     addresses = adr.get_addresses_within_distance(db, latitude, longitude, distance)
+    if not addresses:
+        raise HTTPException(status_code=201, detail="No address found") 
     return addresses
-
